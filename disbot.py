95e8b231dcfdb76c7aaf20a9fcd415f9bdd0b084
RF-Studio <48565867+RF-Studio@users.noreply.github.com>
2020-04-11 13:26:01 +0200

    adding time data on logs

--- disbot.py
+++ disbot.py
@@ -2,6 +2,7 @@ from chan import Chan, ChanError
 from discord.ext import commands
 from discord import Embed
 from html2text import html2text
+from datetime import datetime
 
 chan = Chan()
 
@@ -12,42 +13,50 @@ Valid Commands:
     echo <string>: displays a the provided sting
     post: displays a random post from 4chan
     post <board>: displays a random post from the specified 4chan board
-    disp <thread URL> : displays the thread your enter the URL
+    info <board>: 
 """
 
 
 @commands.command()
 async def echo(ctx, *args):
-    print('echo requested : '+args)
+    print(get_time()+'| echo requested : ' + args)
     await ctx.send(" ".join(args))
 
 
 @commands.command()
 async def hp(ctx):
-    print('Help requested')
+    print(get_time()+'| Help requested')
     await ctx.send(HELPSTRING)
 
 
 @commands.command()
 async def post(ctx, arg=""):
     try:
-        print('Post requested on board : '+arg)
+        print(get_time()+"| Post requested on board : " + arg)
         display_post = chan.get_random_post(arg)
         await ctx.send(f"{html2text(display_post.get_text())[:250]}{display_post.get_img()}")
     except ChanError as e:
         await ctx.send(e.message)
 
-@commands.command()
-async def disp(ctx, *args):
-    try:
-        """get url and display it"""
-    except ChanError as e:
-        await ctx.send(e.message)
 
 @commands.command()
 async def info(ctx, arg=""):
     try:
-        print("Board"+arg+"info requested")
+        print(get_time()+"| Board " + arg + " info requested")
         await ctx.send(html2text(chan.get_board(arg).get_info()))
     except ChanError as e:
         await ctx.send(e.message)
+
+
+class TimeError(Exception):
+    message = "Error getting time"
+
+
+def get_time():  # getting time
+    try:
+        t = str(datetime.now())
+        nt = ""
+        for i in range(19):
+            nt[i] = t[i]
+    except TimeError as e:
+        print(e.message)
